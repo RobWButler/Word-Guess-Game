b7b5d132eb18b067e329b1622460459dbf003482
Robert Butler <robwbutler@gmail.com>
2019-03-03 18:40:46 -0500

    html and other stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,19 +20,24 @@ var answer = "Congratulations!"
 
 document.onkeyup = function(event) {
 
-    var letter = event.key.toLowerCase();
+    var letter = event.key;
 
     var n = answer.includes(letter);
 
     if (n) {
         guesses.rightGuess();
+        document.getElementById("right").innerHTML = guesses.correct;
     }
 
     else {
         guesses.wrongGuess();
+        document.getElementById("wrong").innerHTML = guesses.incorrect;
+
     };
 
     guesses.total();
+    document.getElementById("total").innerHTML = guesses.totalguess;
+
 
     console.log(guesses.correct, guesses.incorrect, guesses.totalguess);
 
